d8f55c9c60e4d6838be12b1ffc19e07a3954a9d9
Prakshal Jain <Prakshal.net12345@gmail.com>
2024-05-29 20:51:10 +0530

    navicons

--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -1,10 +1,23 @@
-
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { useState } from 'react'
 
 const Navicons = () => {
+  const [isProfileOpen,setIsProfileOpen]=useState(false);
+  const [isCartOpen,setIsCartOpen]=useState(false);
   return (
-    <div>
-      NavIcons
+    <div className='flex items-center gap-4 xl:gap-6'>
+      <Image src="/profile.png" alt="" width={22} height={22} className='cursor-pointer' 
+      />
+      {isProfileOpen && (
+      <div>
+        <Link href="/">Profile</Link>
+        <div className='mt-2 cursor-pointer'>Logout</div>
+      </div>
+    )}
+      <Image src="/notification.png" alt="" width={22} height={22} className='cursor-pointer'/>
+      <Image src="/cart.png" alt="" width={22} height={22} className='cursor-pointer'/>
     </div>
   )
 }
